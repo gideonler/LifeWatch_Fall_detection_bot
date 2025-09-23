304f7bcbf06a6ca65add1e8196528da0e241f4cf
Xin Yi <08tanx@gmail.com>
2025-09-23 20:34:54 +0800

    app: add app.py

---
+++ src/app/app.py
@@ -0,0 +1,4 @@
+# TODO decide on tech stack of app (streamlit?)
+# App should provide interface for user to configure email and phone number to send alerts
+# App should allow user to customise prompt/ scenarios to monitor (?)
+# App should provide interface to interact with agent through voice
